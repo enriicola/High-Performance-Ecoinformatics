93f00d7b1ec525d4553f1e7adbfb4bf344ed4a6c
enriicola <s4825087@studenti.unige.it>
2026-05-19 15:48:01 +0200

    refactor

--- run-lucia.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=lucia
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-
-singularity exec --bind $PWD:/work $PWD/containers/geospatial.sif Rscript /work/main.r
---
+++ scripts/run-dago.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=r_singularity
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=56
+#SBATCH --partition=dcgp_usr_prod
+#SBATCH --qos=dcg
+#SBATCH --time=02:00:00
+#SBATCH --output=job.out
+#SBATCH --error=job.err
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+singularity exec --bind $PWD:/work $PWD/containers/geospatial.sif Rscript /work/main.r
