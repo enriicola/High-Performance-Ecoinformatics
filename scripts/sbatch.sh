61b91d7f7b4746f1e000fd3d8e5f0025a3a14680
enriicola <s4825087@studenti.unige.it>
2026-06-09 16:00:30 +0200

    feat: normalize sbatch script for singularity job execution

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=r_singularity
+#SBATCH --account=IscrC_SPECC
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=56
 #SBATCH --partition=dcgp_usr_prod
-#SBATCH --qos=dcg
+#SBATCH --qos=dcgp_qos_bprod
 #SBATCH --time=02:00:00
 #SBATCH --output=job.out
 #SBATCH --error=job.err
 
-cd $WORK
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 time singularity exec --bind $PWD:/work $PWD/containers/geospatial.sif Rscript /work/main.r
