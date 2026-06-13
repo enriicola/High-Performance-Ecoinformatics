d880fbe545ebf092910f789824925276e593ba07
enriicola <s4825087@studenti.unige.it>
2026-06-13 16:19:46 +0200

    fix(slurm): correct script path r/ -> R/ (dir renamed in 9427d4d)

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -14,4 +14,4 @@
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-time singularity exec --pwd /work --bind $PWD:/work $PWD/container/geospatial.sif Rscript "/work/r/new.ensamble_modelling.R"
+time singularity exec --pwd /work --bind $PWD:/work $PWD/container/geospatial.sif Rscript "/work/R/new.ensamble_modelling.R"
