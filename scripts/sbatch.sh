c23622ddabc8b788945643553b2da249e319b169
enriicola <s4825087@studenti.unige.it>
2026-07-05 11:19:40 +0200

    test: request 4 CPUs per SJF job

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -4,7 +4,7 @@
 #SBATCH --array=1-5
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=112
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=0
 #SBATCH --partition=dcgp_usr_prod
 #SBATCH --qos=dcgp_qos_lprod
